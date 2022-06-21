cd069f20f5d24129d794d14cd76c238eef3dd865
skoulouzis <skoulouzis@users.noreply.github.com>
2022-06-21 13:58:37 +0200

    fetch-tiles-01-06-22 update

--- fetch-tiles-01-06-22/fetch-tiles-01-06-22.py
+++ fetch-tiles-01-06-22/fetch-tiles-01-06-22.py
@@ -1,13 +1,13 @@
 from laserfarm.remote_utils import get_wdclient
-import fnmatch
 import pathlib
 from laserfarm.remote_utils import list_remote
+import fnmatch
 import argparse
 arg_parser = argparse.ArgumentParser()
 
 arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
 
-arg_parser.add_argument('--remote_path_retiled', action='store' , type=str , required='True', dest='remote_path_retiled')
+arg_parser.add_argument('--remote_path_retiled', action='store' , required='True', dest='remote_path_retiled')
 
 arg_parser.add_argument('--param_hostname', action='store', type=str, required='True', dest='param_hostname')
 arg_parser.add_argument('--param_login', action='store', type=str, required='True', dest='param_login')
@@ -23,11 +23,11 @@ param_hostname = args.param_hostname
 param_login = args.param_login
 param_password = args.param_password
 
-conf_wd_opts = { 'webdav_hostname': param_hostname, 'webdav_login': param_login, 'webdav_password': param_password}
 conf_remote_path_retiled = pathlib.Path('/webdav/retiled/')
+conf_wd_opts = { 'webdav_hostname': param_hostname, 'webdav_login': param_login, 'webdav_password': param_password}
 
-conf_wd_opts = { 'webdav_hostname': param_hostname, 'webdav_login': param_login, 'webdav_password': param_password}
 conf_remote_path_retiled = pathlib.Path('/webdav/retiled/')
+conf_wd_opts = { 'webdav_hostname': param_hostname, 'webdav_login': param_login, 'webdav_password': param_password}
 remote_path_retiled
 tiles = [t.strip('/') for t in list_remote(get_wdclient(conf_wd_opts), conf_remote_path_retiled.as_posix())
          if fnmatch.fnmatch(t, 'tile_*_*/')]
