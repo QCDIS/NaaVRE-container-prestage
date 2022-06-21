3ca497564ea4bb42e4a01bb47c8cf34d98987ca4
skoulouzis <skoulouzis@users.noreply.github.com>
2022-06-21 13:59:34 +0200

    fetch-norm-tiles-01-06-22 update

--- fetch-norm-tiles-01-06-22/fetch-norm-tiles-01-06-22.py
+++ fetch-norm-tiles-01-06-22/fetch-norm-tiles-01-06-22.py
@@ -1,7 +1,7 @@
 from laserfarm.remote_utils import get_wdclient
-import fnmatch
 import pathlib
 from laserfarm.remote_utils import list_remote
+import fnmatch
 import argparse
 arg_parser = argparse.ArgumentParser()
 
@@ -23,11 +23,11 @@ param_hostname = args.param_hostname
 param_login = args.param_login
 param_password = args.param_password
 
-conf_remote_path_norm = pathlib.Path('/webdav/norm/')
 conf_wd_opts = { 'webdav_hostname': param_hostname, 'webdav_login': param_login, 'webdav_password': param_password}
+conf_remote_path_norm = pathlib.Path('/webdav/norm/')
 
-conf_remote_path_norm = pathlib.Path('/webdav/norm/')
 conf_wd_opts = { 'webdav_hostname': param_hostname, 'webdav_login': param_login, 'webdav_password': param_password}
+conf_remote_path_norm = pathlib.Path('/webdav/norm/')
 remote_path_norm
 norm_tiles = [t.strip('/') for t in list_remote(get_wdclient(conf_wd_opts), conf_remote_path_norm.as_posix())
          if fnmatch.fnmatch(t, 'tile_*_*.laz')]
