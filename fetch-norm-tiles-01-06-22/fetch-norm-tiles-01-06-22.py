c5ccc42dc633df67dfb1c3900b63b6bb85460904
skoulouzis <skoulouzis@users.noreply.github.com>
2022-06-21 18:23:51 +0200

    fetch-norm-tiles-01-06-22 update

--- fetch-norm-tiles-01-06-22/fetch-norm-tiles-01-06-22.py
+++ fetch-norm-tiles-01-06-22/fetch-norm-tiles-01-06-22.py
@@ -1,7 +1,7 @@
 from laserfarm.remote_utils import get_wdclient
-import pathlib
 from laserfarm.remote_utils import list_remote
 import fnmatch
+import pathlib
 import argparse
 arg_parser = argparse.ArgumentParser()
 
@@ -12,6 +12,7 @@ arg_parser.add_argument('--remote_path_norm', action='store' , type=str , requir
 arg_parser.add_argument('--param_hostname', action='store', type=str, required='True', dest='param_hostname')
 arg_parser.add_argument('--param_login', action='store', type=str, required='True', dest='param_login')
 arg_parser.add_argument('--param_password', action='store', type=str, required='True', dest='param_password')
+arg_parser.add_argument('--param_remote_path_root', action='store', type=str, required='True', dest='param_remote_path_root')
 
 args = arg_parser.parse_args()
 
@@ -22,12 +23,13 @@ remote_path_norm = args.remote_path_norm
 param_hostname = args.param_hostname
 param_login = args.param_login
 param_password = args.param_password
+param_remote_path_root = args.param_remote_path_root
 
+conf_remote_path_norm = pathlib.Path(param_remote_path_root + '/norm/')
 conf_wd_opts = { 'webdav_hostname': param_hostname, 'webdav_login': param_login, 'webdav_password': param_password}
-conf_remote_path_norm = pathlib.Path('/webdav/norm/')
 
+conf_remote_path_norm = pathlib.Path(param_remote_path_root + '/norm/')
 conf_wd_opts = { 'webdav_hostname': param_hostname, 'webdav_login': param_login, 'webdav_password': param_password}
-conf_remote_path_norm = pathlib.Path('/webdav/norm/')
 remote_path_norm
 norm_tiles = [t.strip('/') for t in list_remote(get_wdclient(conf_wd_opts), conf_remote_path_norm.as_posix())
          if fnmatch.fnmatch(t, 'tile_*_*.laz')]
