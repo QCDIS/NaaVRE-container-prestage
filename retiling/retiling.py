544e1bb96462fa347c90897db43b75020ca0c96e
skoulouzis <skoulouzis@users.noreply.github.com>
2022-06-22 12:39:53 +0200

    retiling update

--- retiling/retiling.py
+++ retiling/retiling.py
@@ -1,42 +1,60 @@
 from laserfarm import Retiler
-import argparse
 import pathlib
-
+import argparse
 arg_parser = argparse.ArgumentParser()
 
-arg_parser.add_argument('--laz_files', action='store' , type= , required='True', dest='laz_files')
+arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
 
-arg_parser.add_argument('--param_webdav_token', action='store', type=, required='True', dest='param_webdav_token')
+arg_parser.add_argument('--split_laz_files', action='store' , required='True', dest='split_laz_files')
+
+arg_parser.add_argument('--param_hostname', action='store', type=str, required='True', dest='param_hostname')
+arg_parser.add_argument('--param_login', action='store', type=str, required='True', dest='param_login')
+arg_parser.add_argument('--param_max_x', action='store', type=str, required='True', dest='param_max_x')
+arg_parser.add_argument('--param_max_y', action='store', type=str, required='True', dest='param_max_y')
+arg_parser.add_argument('--param_min_x', action='store', type=str, required='True', dest='param_min_x')
+arg_parser.add_argument('--param_min_y', action='store', type=str, required='True', dest='param_min_y')
+arg_parser.add_argument('--param_n_tiles_side', action='store', type=str, required='True', dest='param_n_tiles_side')
+arg_parser.add_argument('--param_password', action='store', type=str, required='True', dest='param_password')
+arg_parser.add_argument('--param_remote_path_root', action='store', type=str, required='True', dest='param_remote_path_root')
 
 args = arg_parser.parse_args()
-laz_files = args.laz_files
 
-param_webdav_token = args.param_webdav_token
+id = args.id
 
-conf_remote_path_ahn = pathlib.Path('/pnfs/grid.sara.nl/data/projects.nl/eecolidar/02_UvA/YShi/Spain/Sevilla')
-conf_remote_path_retiled = pathlib.Path('/pnfs/grid.sara.nl/data/projects.nl/eecolidar/02_UvA/retiled')
-conf_wd_opts = {'webdav_hostname': 'https://webdav.grid.surfsara.nl:2880', 'webdav_token': param_webdav_token}
+split_laz_files = args.split_laz_files
+
+param_hostname = args.param_hostname
+param_login = args.param_login
+param_max_x = args.param_max_x
+param_max_y = args.param_max_y
+param_min_x = args.param_min_x
+param_min_y = args.param_min_y
+param_n_tiles_side = args.param_n_tiles_side
+param_password = args.param_password
+param_remote_path_root = args.param_remote_path_root
+
 conf_local_tmp = pathlib.Path('/tmp')
+conf_remote_path_split = pathlib.Path(param_remote_path_root + '/split')
+conf_wd_opts = { 'webdav_hostname': param_hostname, 'webdav_login': param_login, 'webdav_password': param_password}
+conf_remote_path_retiled = pathlib.Path(param_remote_path_root + '/retiled/')
 
-conf_remote_path_ahn = pathlib.Path('/pnfs/grid.sara.nl/data/projects.nl/eecolidar/02_UvA/YShi/Spain/Sevilla')
-conf_remote_path_retiled = pathlib.Path('/pnfs/grid.sara.nl/data/projects.nl/eecolidar/02_UvA/retiled')
-conf_wd_opts = {'webdav_hostname': 'https://webdav.grid.surfsara.nl:2880', 'webdav_token': param_webdav_token}
 conf_local_tmp = pathlib.Path('/tmp')
-
-remote_path_retiled = conf_remote_path_retiled
+conf_remote_path_split = pathlib.Path(param_remote_path_root + '/split')
+conf_wd_opts = { 'webdav_hostname': param_hostname, 'webdav_login': param_login, 'webdav_password': param_password}
+conf_remote_path_retiled = pathlib.Path(param_remote_path_root + '/retiled/')
+remote_path_retiled = str(conf_remote_path_retiled)
 
 grid_retile = {
-    'min_x': -113107.81,
-    'max_x': 398892.19,
-    'min_y': 214783.87,
-    'max_y': 726783.87,
-    'n_tiles_side': 512
+    'min_x': float(param_min_x),
+    'max_x': float(param_max_x),
+    'min_y': float(param_min_y),
+    'max_y': float(param_max_y),
+    'n_tiles_side': int(param_n_tiles_side)
 }
-
 
 retiling_input = {
     'setup_local_fs': {'tmp_folder': conf_local_tmp.as_posix()},
-    'pullremote': conf_remote_path_ahn.as_posix(),
+    'pullremote': conf_remote_path_split.as_posix(),
     'set_grid': grid_retile,
     'split_and_redistribute': {},
     'validate': {},
@@ -44,11 +62,13 @@ retiling_input = {
     'cleanlocalfs': {}
 }
 
-file = laz_files
-retiler = Retiler(file).config(retiling_input).setup_webdav_client(conf_wd_opts)
-retiler.run()
+    
+file = split_laz_files
+retiler = Retiler(file.replace('"',''),label=file).config(retiling_input).setup_webdav_client(conf_wd_opts)
+retiler_output = retiler.run()
 
 import json
-outs = {}
-outs['remote_path_retiled'] = str(remote_path_retiled)
-print(json.dumps(outs))
+filename = "/tmp/remote_path_retiled_" + id + ".json"
+file_remote_path_retiled = open(filename, "w")
+file_remote_path_retiled.write(json.dumps(remote_path_retiled))
+file_remote_path_retiled.close()
