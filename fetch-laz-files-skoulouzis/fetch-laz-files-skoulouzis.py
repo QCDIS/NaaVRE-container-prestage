50d7d0a85accc44c6185d170dd1f8d79e2e958d4
skoulouzis <skoulouzis@users.noreply.github.com>
2022-06-23 15:37:19 +0200

    fetch-laz-files-skoulouzis update

--- fetch-laz-files-skoulouzis/fetch-laz-files-skoulouzis.py
+++ fetch-laz-files-skoulouzis/fetch-laz-files-skoulouzis.py
@@ -1,4 +1,5 @@
 from laserfarm.remote_utils import get_wdclient
+import os
 import pathlib
 from laserfarm.remote_utils import list_remote
 import argparse
@@ -10,7 +11,7 @@ arg_parser.add_argument('--id', action='store', type=str, required=True, dest='i
 arg_parser.add_argument('--param_hostname', action='store', type=str, required='True', dest='param_hostname')
 arg_parser.add_argument('--param_login', action='store', type=str, required='True', dest='param_login')
 arg_parser.add_argument('--param_password', action='store', type=str, required='True', dest='param_password')
-arg_parser.add_argument('--param_remote_path_ahn', action='store', type=str, required='True', dest='param_remote_path_ahn')
+arg_parser.add_argument('--param_remote_path_root', action='store', type=str, required='True', dest='param_remote_path_root')
 
 args = arg_parser.parse_args()
 
@@ -20,13 +21,16 @@ id = args.id
 param_hostname = args.param_hostname
 param_login = args.param_login
 param_password = args.param_password
-param_remote_path_ahn = args.param_remote_path_ahn
+param_remote_path_root = args.param_remote_path_root
 
+conf_remote_path_ahn = os.path.join(param_remote_path_root,'ahn')
 conf_wd_opts = { 'webdav_hostname': param_hostname, 'webdav_login': param_login, 'webdav_password': param_password}
 
+conf_remote_path_ahn = os.path.join(param_remote_path_root,'ahn')
 conf_wd_opts = { 'webdav_hostname': param_hostname, 'webdav_login': param_login, 'webdav_password': param_password}
-laz_files = [f for f in list_remote(get_wdclient(conf_wd_opts), pathlib.Path(param_remote_path_ahn).as_posix())
+laz_files = [f for f in list_remote(get_wdclient(conf_wd_opts), pathlib.Path(conf_remote_path_ahn).as_posix())
              if f.lower().endswith('.laz')]
+print(laz_files)
 
 import json
 filename = "/tmp/laz_files_" + id + ".json"
