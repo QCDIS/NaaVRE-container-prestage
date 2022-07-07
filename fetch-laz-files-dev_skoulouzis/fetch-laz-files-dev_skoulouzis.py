078393dce7ee3ed84a973731066064da96c691ba
skoulouzis <skoulouzis@users.noreply.github.com>
2022-07-07 15:02:04 +0200

    fetch-laz-files-dev_skoulouzis update

--- fetch-laz-files-dev_skoulouzis/fetch-laz-files-dev_skoulouzis.py
+++ fetch-laz-files-dev_skoulouzis/fetch-laz-files-dev_skoulouzis.py
@@ -1,5 +1,5 @@
-import pathlib
 from laserfarm.remote_utils import get_wdclient
+import pathlib
 from laserfarm.remote_utils import list_remote
 import argparse
 arg_parser = argparse.ArgumentParser()
