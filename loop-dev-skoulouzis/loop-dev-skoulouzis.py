da46329e7471ac7df113b5a271759b5442bd616c
skoulouzis <skoulouzis@users.noreply.github.com>
2022-07-08 21:40:54 +0200

    loop-dev-skoulouzis update

--- loop-dev-skoulouzis/loop-dev-skoulouzis.py
+++ loop-dev-skoulouzis/loop-dev-skoulouzis.py
@@ -1,14 +1,13 @@
 import argparse
 arg_parser = argparse.ArgumentParser()
-
 arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
-
-arg_parser.add_argument('--laz_files', action='store' , required='True', dest='laz_files')
-
-
 args = arg_parser.parse_args()
 
 id = args.id
+
+
+arg_parser.add_argument('--laz_files', action='store' , required='True', dest='laz_files')
+
 
 laz_files = args.laz_files
 
