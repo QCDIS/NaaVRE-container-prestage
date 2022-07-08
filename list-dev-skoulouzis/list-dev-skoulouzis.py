7fd7696c6e65d78e8c290aa2ad8c9d96508e640c
skoulouzis <skoulouzis@users.noreply.github.com>
2022-07-08 20:01:21 +0200

    list-dev-skoulouzis creation

---
+++ list-dev-skoulouzis/list-dev-skoulouzis.py
@@ -0,0 +1,9 @@
+
+
+laz_files = ['a','b','c']
+
+import json
+filename = "/tmp/laz_files_" + id + ".json"
+file_laz_files = open(filename, "w")
+file_laz_files.write(json.dumps(laz_files))
+file_laz_files.close()
