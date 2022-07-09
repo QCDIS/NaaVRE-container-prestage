f4743fb595567a224a0783ffde867b6e7a4de91f
skoulouzis <skoulouzis@users.noreply.github.com>
2022-07-09 12:56:35 +0200

    a-list-dev-skoulouzis update

--- a-list-dev-skoulouzis/a-list-dev-skoulouzis.py
+++ a-list-dev-skoulouzis/a-list-dev-skoulouzis.py
@@ -2,13 +2,13 @@ import argparse
 arg_parser = argparse.ArgumentParser()
 
 arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
-args = arg_parser.parse_args()
-
-id = args.id
-
 
 
 arg_parser.add_argument('--param_a', action='store', type=int, required='True', dest='param_a')
+
+args = arg_parser.parse_args()
+
+id = args.id
 
 
 param_a = args.param_a
