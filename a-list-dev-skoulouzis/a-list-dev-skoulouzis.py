08674a5628a463c617972176d976b651df5acd69
skoulouzis <skoulouzis@users.noreply.github.com>
2022-07-09 12:06:54 +0200

    a-list-dev-skoulouzis update

--- a-list-dev-skoulouzis/a-list-dev-skoulouzis.py
+++ a-list-dev-skoulouzis/a-list-dev-skoulouzis.py
@@ -8,7 +8,14 @@ id = args.id
 
 
 
+arg_parser.add_argument('--param_a', action='store', type=int, required='True', dest='param_a')
 
+
+param_a = args.param_a
+
+
+
+print(param_a)
 laz_files = ['1','2','3']
 
 import json
