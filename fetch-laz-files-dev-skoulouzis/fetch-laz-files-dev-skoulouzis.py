f55154cf5ae12c8b565c5d34c0beaa35e2585e36
skoulouzis <skoulouzis@users.noreply.github.com>
2022-07-10 23:52:28 +0200

    fetch-laz-files-dev-skoulouzis update

--- fetch-laz-files-dev-skoulouzis/fetch-laz-files-dev-skoulouzis.py
+++ fetch-laz-files-dev-skoulouzis/fetch-laz-files-dev-skoulouzis.py
@@ -1,12 +1,12 @@
-from laserfarm.remote_utils import get_wdclient
 from laserfarm.remote_utils import list_remote
+from laserfarm.remote_utils import get_wdclient
 import pathlib
 
-conf_remote_path_ahn = conf_remote_path_root + '/ahn'
 conf_wd_opts = { 'webdav_hostname': conf_hostname, 'webdav_login': conf_login, 'webdav_password': conf_password}
+conf_remote_path_ahn = conf_remote_path_root + '/ahn'
 
-conf_remote_path_ahn = conf_remote_path_root + '/ahn'
 conf_wd_opts = { 'webdav_hostname': conf_hostname, 'webdav_login': conf_login, 'webdav_password': conf_password}
+conf_remote_path_ahn = conf_remote_path_root + '/ahn'
 print(conf_remote_path_ahn)
 laz_files = [f for f in list_remote(get_wdclient(conf_wd_opts), pathlib.Path(conf_remote_path_ahn).as_posix())
              if f.lower().endswith('.laz')]
