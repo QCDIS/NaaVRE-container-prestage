ebb302ffcb7f0d86daadbbaa74e22198b92f303a
skoulouzis <skoulouzis@users.noreply.github.com>
2022-07-10 23:53:11 +0200

    retiling-dev-skoulouzis update

--- retiling-dev-skoulouzis/retiling-dev-skoulouzis.py
+++ retiling-dev-skoulouzis/retiling-dev-skoulouzis.py
@@ -1,43 +1,42 @@
-from laserfarm import Retiler
 import pathlib
+from laserfarm import Retiler
 import argparse
 arg_parser = argparse.ArgumentParser()
 
 arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
 
-arg_parser.add_argument('--laz_files', action='store' , required='True', dest='laz_files')
 arg_parser.add_argument('--split_laz_files', action='store' , required='True', dest='split_laz_files')
 
+arg_parser.add_argument('--param_username', action='store', type=str, required='True', dest='param_username')
 
 args = arg_parser.parse_args()
 
 id = args.id
 
 import json
-laz_files = json.loads(args.laz_files)
-import json
 split_laz_files = json.loads(args.split_laz_files)
 
+param_username = args.param_username
 
-conf_n_tiles_side = '512'
-conf_remote_path_retiled = pathlib.Path(conf_remote_path_root + '/retiled_'+username)
 conf_min_y = '214783.87'
-conf_remote_path_split = pathlib.Path(conf_remote_path_root + '/split_'+username)
-conf_local_tmp = pathlib.Path('/tmp')
-conf_min_x = '-113107.81'
-conf_max_x = '398892.19'
 conf_max_y = '726783.87'
 conf_wd_opts = { 'webdav_hostname': conf_hostname, 'webdav_login': conf_login, 'webdav_password': conf_password}
-
-conf_n_tiles_side = '512'
-conf_remote_path_retiled = pathlib.Path(conf_remote_path_root + '/retiled_'+username)
-conf_min_y = '214783.87'
-conf_remote_path_split = pathlib.Path(conf_remote_path_root + '/split_'+username)
-conf_local_tmp = pathlib.Path('/tmp')
-conf_min_x = '-113107.81'
 conf_max_x = '398892.19'
+conf_remote_path_retiled = pathlib.Path(conf_remote_path_root + '/retiled_'+param_username)
+conf_remote_path_split = pathlib.Path(conf_remote_path_root + '/split_'+param_username)
+conf_min_x = '-113107.81'
+conf_n_tiles_side = '512'
+conf_local_tmp = pathlib.Path('/tmp')
+
+conf_min_y = '214783.87'
 conf_max_y = '726783.87'
 conf_wd_opts = { 'webdav_hostname': conf_hostname, 'webdav_login': conf_login, 'webdav_password': conf_password}
+conf_max_x = '398892.19'
+conf_remote_path_retiled = pathlib.Path(conf_remote_path_root + '/retiled_'+param_username)
+conf_remote_path_split = pathlib.Path(conf_remote_path_root + '/split_'+param_username)
+conf_min_x = '-113107.81'
+conf_n_tiles_side = '512'
+conf_local_tmp = pathlib.Path('/tmp')
 split_laz_files
 remote_path_retiled = str(conf_remote_path_retiled)
 
@@ -59,7 +58,7 @@ retiling_input = {
     'cleanlocalfs': {}
 }
 
-for file in laz_files:
+for file in split_laz_files:
     retiler = Retiler(file.replace('"',''),label=file).config(retiling_input).setup_webdav_client(conf_wd_opts)
     retiler_output = retiler.run()
 
