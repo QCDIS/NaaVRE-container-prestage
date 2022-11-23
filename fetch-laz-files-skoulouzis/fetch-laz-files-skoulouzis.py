84b1cfa8628dbfe9542a71746694e2f082113703
skoulouzis <skoulouzis@users.noreply.github.com>
2022-11-23 19:24:58 +0100

    fetch-laz-files-skoulouzis update

--- fetch-laz-files-skoulouzis/fetch-laz-files-skoulouzis.py
+++ fetch-laz-files-skoulouzis/fetch-laz-files-skoulouzis.py
@@ -1,6 +1,6 @@
-import pathlib
 from laserfarm.remote_utils import get_wdclient
 from laserfarm.remote_utils import list_remote
+import pathlib
 
 import argparse
 arg_parser = argparse.ArgumentParser()
@@ -8,18 +8,24 @@ arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
 
 
+arg_parser.add_argument('--param_hostname', action='store', type=str, required='True', dest='param_hostname')
+arg_parser.add_argument('--param_login', action='store', type=str, required='True', dest='param_login')
+arg_parser.add_argument('--param_password', action='store', type=str, required='True', dest='param_password')
 
 args = arg_parser.parse_args()
 
 id = args.id
 
 
+param_hostname = args.param_hostname
+param_login = args.param_login
+param_password = args.param_password
 
+conf_wd_opts = { 'webdav_hostname': param_hostname, 'webdav_login': param_login, 'webdav_password': param_password}
 conf_remote_path_ahn =  '/webdav/LAZ'
-conf_wd_opts = { 'webdav_hostname':  'https://lfw-ds001-i022.lifewatch.dev:32443/', 'webdav_login':  '20BNXDdL8mg24OaD', 'webdav_password':  'zDoy0hNKkcnsdsQ@OYAVd'}
 
+conf_wd_opts = { 'webdav_hostname': param_hostname, 'webdav_login': param_login, 'webdav_password': param_password}
 conf_remote_path_ahn =  '/webdav/LAZ'
-conf_wd_opts = { 'webdav_hostname':  'https://lfw-ds001-i022.lifewatch.dev:32443/', 'webdav_login':  '20BNXDdL8mg24OaD', 'webdav_password':  'zDoy0hNKkcnsdsQ@OYAVd'}
 print(conf_remote_path_ahn)
 laz_files = [f for f in list_remote(get_wdclient(conf_wd_opts), pathlib.Path(conf_remote_path_ahn).as_posix())
              if f.lower().endswith('.laz')]
