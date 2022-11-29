66ffe5ea6c641191b1ad7bf63f1092e72a943ec7
skoulouzis <skoulouzis@users.noreply.github.com>
2022-11-29 16:18:29 +0100

    loop-nali001 update

--- loop-nali001/loop-nali001.py
+++ loop-nali001/loop-nali001.py
@@ -6,6 +6,7 @@ arg_parser.add_argument('--id', action='store', type=str, required=True, dest='i
 
 arg_parser.add_argument('--l', action='store', type=list, required='True', dest='l')
 
+arg_parser.add_argument('--param_user', action='store', type=str, required='True', dest='param_user')
 
 args = arg_parser.parse_args()
 
@@ -13,8 +14,10 @@ id = args.id
 
 l = args.l
 
+param_user = args.param_user
 
 
 for i in l: 
+    print(param_user)
     print(i)
 
