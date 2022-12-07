352c093a3eddd1264a711520501b9b7a3c8c5879
skoulouzis <skoulouzis@users.noreply.github.com>
2022-12-07 10:05:48 +0100

    define-region-of-interest-joristimmermans update

--- define-region-of-interest-joristimmermans/define-region-of-interest-joristimmermans.py
+++ define-region-of-interest-joristimmermans/define-region-of-interest-joristimmermans.py
@@ -15,14 +15,11 @@ id = args.id
 
 
 roi = 'POLYGON ((5.163574 52.382529, 5.163574 52.529813, 5.493164 52.529813, 5.493164 52.382529, 5.163574 52.382529))'
-roi_grid = 'EPSG:4326'
+spatial_resolution = 20
+
 
 import json
 filename = "/tmp/roi_" + id + ".json"
 file_roi = open(filename, "w")
 file_roi.write(json.dumps(roi))
 file_roi.close()
-filename = "/tmp/roi_grid_" + id + ".json"
-file_roi_grid = open(filename, "w")
-file_roi_grid.write(json.dumps(roi_grid))
-file_roi_grid.close()
