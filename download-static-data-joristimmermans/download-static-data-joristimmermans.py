a08aab2b5bad246f3ce5809eec219d0c52ec64a9
skoulouzis <skoulouzis@users.noreply.github.com>
2022-12-07 10:00:35 +0100

    download-static-data-joristimmermans update

--- download-static-data-joristimmermans/download-static-data-joristimmermans.py
+++ download-static-data-joristimmermans/download-static-data-joristimmermans.py
@@ -1,18 +1,13 @@
 from multiply_data_access import DataAccessComponent
 from vm_support.utils import set_permissions
 from vm_support.sym_linker import create_sym_links
+import os
 
 import argparse
 arg_parser = argparse.ArgumentParser()
 
 arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
 
-
-arg_parser.add_argument('--create_dir', action='store', type=str, required='True', dest='create_dir')
-
-arg_parser.add_argument('--dem_directory', action='store', type=str, required='True', dest='dem_directory')
-
-arg_parser.add_argument('--emulators_directory', action='store', type=str, required='True', dest='emulators_directory')
 
 arg_parser.add_argument('--roi', action='store', type=str, required='True', dest='roi')
 
@@ -22,19 +17,20 @@ arg_parser.add_argument('--start_time_as_string', action='store', type=str, requ
 
 arg_parser.add_argument('--stop_time_as_string', action='store', type=str, required='True', dest='stop_time_as_string')
 
+arg_parser.add_argument('--working_dir', action='store', type=str, required='True', dest='working_dir')
+
 
 args = arg_parser.parse_args()
 print(args)
 
 id = args.id
 
-create_dir = args.create_dir
-dem_directory = args.dem_directory
-emulators_directory = args.emulators_directory
 roi = args.roi
 roi_grid = args.roi_grid
 start_time_as_string = args.start_time_as_string
 stop_time_as_string = args.stop_time_as_string
+working_dir = args.working_dir
+
 
 
 
@@ -57,18 +53,20 @@ def get_static_data(data_access_component: DataAccessComponent, roi: str, roi_gr
     set_permissions(dem_urls)
     create_sym_links(dem_urls, dem_directory)
     print('Done retrieving static data')
-    
+
+def create_dir(dir):
+    try:
+        if not os.path.exists(dir):
+            os.makedirs(dir)
+    except:
+        print(dir)
+    return
+emulators_directory = '{}/emulators'.format(working_dir)
+dem_directory = '{}/dem'.format(working_dir)
+
 data_access_component = DataAccessComponent()
 
 get_static_data(data_access_component=data_access_component, roi=roi,
                 start_time=start_time_as_string, stop_time=stop_time_as_string, 
           emulation_directory=emulators_directory, dem_directory=dem_directory, roi_grid=roi_grid)
 
-
-
-
-import json
-filename = "/tmp/data_access_component_" + id + ".json"
-file_data_access_component = open(filename, "w")
-file_data_access_component.write(json.dumps(data_access_component))
-file_data_access_component.close()
